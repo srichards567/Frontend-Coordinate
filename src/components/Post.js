a38dbe7c80fa1a4bdd90076378601b42d9f99c42
Samantha Richards <srichards567@gmail.com>
2019-11-14 20:24:17 -0500

    updated post component.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,28 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Post = {( post, vote, index )} => {
+class Post extends Component {
+  render() {
+
   return (
-    <div className="post">
-      <div className="vote">
-        <div className="upVote" onClick={() =>(post._id, index, true)}>
-          <span role="img" aria-label="up vote">
-            up arrow
-          </span>
-        </div>
-        <div className="votecount">{post.votes}</div>
-        <div className="downvote" onClick{() => vote(post._id, index, false)}>
-          <span role="img" aria-label="down vote">
-            down arrow
-          </span>
-      </div>
+    <div>
+      <div>{this.props.name}</div>
+      <div>{this.props.description}</div>
     </div>
-
-    <div className="content">
-      <div className="title-area">
-        <span className="title">{post.title}</span>
-      </div>
-    </div>
-  )
+  );
+ }
 }
 
 export default Post;
